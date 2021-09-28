725b6b91c30d871efe3f6ea714d5916af8e193ae
Uladzislau Shablinski <vladshablinsky@gmail.com>
2021-09-28 10:45:46 +0300

    add files from (2021, 9, 28)

---
+++ 1455/c.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+  int x, y;
+  cin >> x >> y;
+  cout << x - 1 << " " << y << "\n";
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
