39575f6b140194f0c84b04d078954bb9d1952fb6
Uladzislau Shablinski <vladshablinsky@gmail.com>
2021-10-30 17:40:55 +0300

    add files from (2021, 10, 30)

---
+++ algo_snippets/lca.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+const int MAXN = 1e5;
+
+int tin[MAXN];
+int tout[MAXN];
+int up[MAXN][MAXP];
+
+
+void dfs() {
+
+}
+
+int main() {
+
+}
