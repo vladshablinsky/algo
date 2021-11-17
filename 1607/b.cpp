fdaa9fbb93d13aebbf70662ce0461d1a491034c2
Uladzislau Shablinski <vladshablinsky@gmail.com>
2021-11-17 21:14:11 +0100

    add files from (2021, 11, 17)

---
+++ 1607/b.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+long long mabs(long long x) {
+  if (x < 0) {
+    return -x;
+  }
+  return x;
+}
+
+
+void solve() {
+  long long x;
+  long long n;
+
+  long long till_zero = (x + 1) / 2;
+
+  long long dist = (n + 1) / 2;
+
+}
+
+int main() {
+  int t;
+  scanf("%d", &t);
+
+}
